3163d70ccfdb76a04b9c29d56abe6eee39fe6201
Gabriel <gabriel01sansan@gmail.com>
2024-05-21 21:36:55 +0200

    Apto para cambiar logos

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    compileSdkVersion(34)
+    defaultConfig {
+        applicationId = "com.gestiona.stockwise"
+        minSdkVersion(24)
+        targetSdkVersion(34)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        multiDexEnabled = true
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    namespace = "com.gestiona.stockwise"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+
+    // Ampliar límite métodos
+    implementation("androidx.multidex:multidex:2.0.1")
+}
---
+++ app/src/main/java/com/gestiona/stockwise/ContactosActivity.java
@@ -0,0 +1,152 @@
+package com.gestiona.stockwise;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class ContactosActivity extends AppCompatActivity {
+
+
+    private TextView tvEmailUsuario, tvNombreEmpresa;
+    private ImageView image;
+    String email, nombreEmpresa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactos);
+
+
+        ImageButton QR = findViewById(R.id.btCompartirAppQR);
+        ImageButton LINK = findViewById(R.id.btCompartirAppLink);
+        Button empresa = findViewById(R.id.btDatosEmpresa);
+
+
+        empresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                email = tvEmailUsuario.getText().toString();
+                nombreEmpresa = tvNombreEmpresa.getText().toString();
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "DATOS EMPRESA: " +
+                        "\n\n\n" +
+                        "EMAIL: " + email + "\n\n" +
+                        "NOMBRE EMPRESA: " + nombreEmpresa); //+" \n" +
+                // "DIRECCIÓN: "+"añadir dirección aquí \n"+
+                //  "TELÉFONO: "+"añadir teléfono aquí");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
+
+        QR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarQR(ContactosActivity.this);
+            }
+        });
+
+        LINK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+//                sendIntent.setPackage("com.whatsapp");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Descarga GESTY a través de este link: \n\n https://play.google.com/store/apps/details?id=com.mariomunozmyaplication.gestionatunegocio");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
+
+        //CARGAR IMAGEN DESARROLLADOR David
+        ImageView david = findViewById(R.id.imgDavid);
+        Glide.with(this)
+                .load(R.drawable.david)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.Teal)))
+                .centerCrop()
+                .into(david);
+
+        //CARGAR IMAGEN DESARROLLADOR Gabriel
+        ImageView gabriel = findViewById(R.id.imgGabriel);
+        Glide.with(this)
+                .load(R.drawable.gabriel)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.Teal)))
+                .centerCrop()
+                .into(gabriel);
+
+
+        image = new ImageView(this);
+        image.setImageResource(R.drawable.linkgesty);
+
+    }
+
+    // Opciones menú 3 puntitos
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, ConfguracionActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    // Metodo para modificar el NavHeader
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        tvEmailUsuario = findViewById(R.id.tvEmailUsuario);
+        tvNombreEmpresa = findViewById(R.id.tvNombreEmpresa);
+
+        // Mostramos el nombre de la empresa y el correo en el menu draw
+        tvEmailUsuario.setText(LoginActivity.user.getEmail());
+        String nombreEmpresa = LoginActivity.user.getDisplayName();
+        if (nombreEmpresa != null && !nombreEmpresa.equals("")) {
+            tvNombreEmpresa.setText(nombreEmpresa);
+        }
+        return true;
+    }
+
+
+    public void cargarQR(ContactosActivity contactos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Escanea el código QR");//TITULO
+        builder.setIcon(R.drawable.lupaqr);//ICONO
+
+        builder.setCancelable(false);
+        builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(ContactosActivity.this, ContactosActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+
+            }
+        }).setView(image);
+        builder.create().show();
+
+    }
+
+
+}
